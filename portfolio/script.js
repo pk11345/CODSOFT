6a64db550b234c3bd5e71871fe2d1a46a988af1f
pk11345 <117341395+pk11345@users.noreply.github.com>
2024-09-18 14:13:43 +0530

    Portfolio

---
+++ portfolio/script.js
@@ -0,0 +1,9 @@
+let button = document.querySelector(".contacts")
+
+let Mobile = 9871493932;
+let Email = "pk11345rai@gmail.com"
+
+button.addEventListener("click", ()=>{
+    alert(`Mobile Number -${Mobile} 
+    EMail id -${Email}`)
+})
